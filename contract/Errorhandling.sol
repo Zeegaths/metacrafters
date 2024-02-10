f2b8209ac13fe2ec093bc4a7d17a47f3517df88a
Zeegaths <zarahgathoni76@gmail.com>
2024-02-11 00:44:50 +0100

    errorhandling

--- contract/Errorhandling.sol
+++ contract/Errorhandling.sol
@@ -1,29 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
  
-contract Errorhandling {
-    uint public balance;
+contract SchoolRegister{
+    address public principal;
 
-    function deposit (uint _amount) public payable{
-        //ensures the user enters a valid amount otherwise revert the transaction
-        require(_amount >= 0, "amount has to be greater than 0");
-        
-        //rolls back the transaction if the condition is not met
-        if (_amount >= 10000) {
-            revert("Amount exceeds maximum limit");
+    struct Teachers{
+        string Name;
+        uint8 Age;
+        address teacherA;
+    }
+
+//mapping address to struct
+    mapping(address => Teachers) register;
+
+    constructor() {
+        principal = msg.sender;
+    }
+
+    function registerTeacher (string memory _Name, uint8 _age, address _teacherAddress) public {
+        require(principal == msg.sender, "You have to be the principal to add a teacher");
+        //ensure the teacher address does not exist
+        assert(register[_teacherAddress].teacherA == address(0));
+        register[_teacherAddress] = Teachers(_Name, _age , _teacherAddress); 
+              
+    }
+
+    function deleteTeacher(address _teacherAddress) public  {
+         if (principal != msg.sender) {
+        revert("Cannot perfom this action");  
         }
-
-        //balance updates if condition are met
-        balance += _amount;
+     delete register[_teacherAddress];
     }
-
-    function withdraw(uint amount) public {
-        // If the amount is greater than the balance, the transaction is reverted.
-        assert(balance >= amount);
-
-        // Updating balance if the assert condition is true
-        balance -= amount;
-    }
-
-
 }
